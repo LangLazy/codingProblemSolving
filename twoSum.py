47262a0535b41e71787b9c8534fbfc5be28158f1
LangLazy <onaverageitsok@gmail.com>
2021-11-07 13:17:07 -0500

    Some leetcode problem

---
+++ twoSum.py
@@ -0,0 +1,12 @@
+class Solution:
+    def twoSum(self, nums: list[int], target: int)-> list[int]:
+        memo ={}
+        for i in range(len(nums)):
+            memo[target - nums[i]] = i
+        for i in range(len(nums)):
+            if (nums[i] in memo) and (i !=memo[nums[i]]) :
+                return [i, memo[nums[i]]]
+
+soln = Solution()
+print(soln.twoSum([2,7,11,15], 9))
+            
